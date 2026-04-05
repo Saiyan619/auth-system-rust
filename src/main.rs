130a912d58186636bf3b047dbe94effd2dbf7cf6
saiyan619 <arokoyueb11@gmail.com>
2026-04-05 14:06:29 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{ Router, routing::get };
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    
+    println!("Server is running on port: 3000");
+    
+    axum::serve(listener, app).await.unwrap()
+
+}
+
+async fn root() -> &'static str {
+    "Server is Live!!!"
+}
