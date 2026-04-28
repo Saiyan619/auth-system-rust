c3ef4c2a54481f89fd6f1f31819e37bffea11088
saiyan619 <arokoyueb11@gmail.com>
2026-04-28 14:22:38 +0100

    buidling login handler function

--- src/handler/auth.rs
+++ src/handler/auth.rs
@@ -1,10 +1,13 @@
-use std::sync::Arc;
+use std::{env, sync::Arc};
 
-use axum::{Extension, Json, body, http::{self, StatusCode}, response::IntoResponse};
+use axum::{Extension, Json, body, http::{self, HeaderMap, StatusCode, header::{self, SET_COOKIE}, response}, response::IntoResponse};
+use axum_extra::extract::cookie::Cookie;
 use chrono::{Duration, Utc};
+use jsonwebtoken::Header;
+use time::Time;
 use validator::Validate;
 
-use crate::{AppState, db::UserExt, dtos::{RegisterUserDto, Response}, errors::{ErrorMessage, HttpError}, mail::mails::send_verification_email, utils::password};
+use crate::{AppState, db::{DbClient, UserExt}, dtos::{LoginUserRequestDto, RegisterUserDto, Response, UserLoginResponseDto}, errors::{ErrorMessage, HttpError}, mail::mails::send_verification_email, utils::{password::{self, compare}, token}};
 
 
 
@@ -40,3 +43,42 @@ pub async fn register(Extension(app_state):Extension<Arc<AppState>>, Json(body):
         Err(e) => Err(HttpError::server_error(e.to_string()))
     }
 }
+
+pub async fn login(Extension(app_state): Extension<Arc<AppState>>, Json(body): Json<LoginUserRequestDto>) 
+-> Result<impl IntoResponse, HttpError> {
+    body.validate().map_err(|e| HttpError::server_error(e.to_string()))?;
+    let result = app_state.db_client.get_user(None, None, Some(&body.email), None)
+    .await
+    .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let user = result.ok_or(HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))?;
+
+    let password = compare(&body.password, &user.password)
+    .map_err(|_| HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))?;
+
+    if password {
+        let token = token::create_token(&user.id.to_string(), &app_state.env.jwt_secret.as_bytes(), app_state.env.jwt_maxage)
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+         
+         let cookie_duration = time::Duration::minutes(app_state.env.jwt_maxage * 60);
+         let cookie = Cookie::build(("token", token.clone())).path("/").max_age(cookie_duration).http_only(true).build();
+
+         let response = axum::response::Json(UserLoginResponseDto{
+            status: "success".to_string(),
+            token: token.to_string()
+         });
+
+         let mut header = HeaderMap::new();
+
+         header.append(header::SET_COOKIE, cookie.to_string().parse().unwrap());
+
+         let mut response = response.into_response();
+
+         response.headers_mut().extend(header);
+
+         Ok(response)
+         }else {
+             Err(HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))
+         }
+    }
+  
